ecb8c967ad24615d2663703efb86c6b4bcf8b7d1
Chris DuBois <chris.dubois@gmail.com>
2012-03-06 23:11:51 -0800

    Change glm to use fifo to interact with named pipes.

--- tests/namedpipe.r
+++ tests/namedpipe.r
@@ -2,10 +2,8 @@
 suppressPackageStartupMessages(library("optparse"))
 
 option_list <- list(
-  make_option("--infile", help=""),
-  make_option("--outfile", help="")
+  make_option("--infile", help="")
   )
 opts   <- parse_args(OptionParser(option_list=option_list))
 
-out <- read.csv(opts$infile)
-write(out,file=opts$outfile)
+print(read.csv(fifo(opts$infile)))
